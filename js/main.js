1b39a089e28599054a0c4d4cbd310507096bc4d2
Courtney Meza <courtneymeza@Courtneys-MacBook-Air.local>
2018-01-07 21:12:07 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [cards.push('cardOne'), cards.push('cardTwo')];
-var cardOne = (cards[0]);
-var cardTwo = (cards[2]);
-if (cardsInPlay.length === 2) {
-  if (cardOne === cardTwo) {alert("You found a match!");
-} else {alert("Sorry, try again.");
+var cardsInPlay = [];
+var checkForMatch = function(){
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    console.log("You found a match!");
+  } else {
+    console.log("Sorry, try again.");
+  }
 }
-}
+var flipCard = function (cardId){
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+};
+flipCard(0);
+flipCard(2);
